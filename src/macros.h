0f60f09b0c7f4e1b6adeb79af52315b903e89042
Jonathan Howard <j@hovverd.com>
2015-09-16 23:07:10 -0400

    added alignment macro

--- src/macros.h
+++ src/macros.h
@@ -2,3 +2,12 @@
 
 /* allows changing later without major refactor */
 #define ASSERT(a) assert(a)
+
+/* alignment macros */
+#if defined(_MSC_VER)
+#define __ALIGNED__(x) __declspec(align(x))
+#else
+#if defined(__GNUC__)
+#define __ALIGNED__(x) __attribute__ ((aligned(x)))
+#endif
+#endif
