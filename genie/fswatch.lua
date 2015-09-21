c42e7bf0c85018f65ccbc87c667147fa93c3ef89
Jonathan Howard <j@hovverd.com>
2015-09-21 15:02:54 -0400

    genie build files for fswatch

---
+++ genie/fswatch.lua
@@ -0,0 +1,57 @@
+--
+-- Copyright (c) 2015 Jonathan Howard
+-- MIT License
+--
+
+function fswatch_library()
+local LIBFSWATCH_SRC_DIR = FSWATCH_DIR .. "libfswatch/src/libfswatch/"
+
+project "fswatch"
+    targetname "fswatch"
+    language "C++"
+    kind "StaticLib"
+
+    removeflags { "NoExceptions" }
+
+    defines
+    {
+        "HAVE_CXX_MUTEX=1",
+        "HAVE_CXX_UNIQUE_PTR=1"
+    }
+
+    files 
+    {
+        LIBFSWATCH_SRC_DIR .. "c++/event.cpp",
+        LIBFSWATCH_SRC_DIR .. "c++/monitor.cpp",
+        LIBFSWATCH_SRC_DIR .. "c++/libfswatch_exception.cpp",
+        LIBFSWATCH_SRC_DIR .. "c++/path_utils.cpp",
+        LIBFSWATCH_SRC_DIR .. "c++/**.h",
+    }
+
+    includedirs
+    {
+        LIBFSWATCH_SRC_DIR .. "/"
+    }
+
+    configuration { "not windows" }
+        files { LIBFSWATCH_SRC_DIR .. "c++/poll_monitor.cpp" }
+
+    configuration { "gmake" }
+        buildoptions { "-std=c++11" }
+
+    configuration { "osx" }
+        linkoptions { "-stdlib=libc++" }
+        files { LIBFSWATCH_SRC_DIR .. "c++/fsevents_monitor.*" }
+
+    configuration { "linux" }
+        defines { "HAVE_STRUCT_STAT_ST_MTIME" }
+        files
+        {
+            LIBFSWATCH_SRC_DIR .. "c++/kqueue_monitor.cpp",
+            LIBFSWATCH_SRC_DIR .. "c++/inotify_monitor.cpp"
+        }
+
+    configuration { "osx or bsd" }
+        defines { "HAVE_STRUCT_STAT_ST_MTIMESPEC" }
+        files { LIBFSWATCH_SRC_DIR .. "c++/fsevents_monitor.cpp" }
+end
--- genie/genie.lua
+++ genie/genie.lua
@@ -10,6 +10,7 @@ local ENG_BUILD_DIR = (ENG_DIR .. "build/")
 BGFX_DIR    = (ENG_EXT_DIR .. "bgfx/") 
 BX_DIR      = (ENG_EXT_DIR .. "bx/")
 ASSIMP_DIR  = (ENG_EXT_DIR .. "assimp/")
+FSWATCH_DIR = (ENG_EXT_DIR .. "fswatch/")
 
 function copyLib()
 end
@@ -41,6 +42,7 @@ dofile ("toolchain.lua")
 dofile (BGFX_DIR .. "scripts/bgfx.lua")
 dofile ("engine.lua")
 dofile ("assimp.lua")
+dofile ("fswatch.lua")
 
 toolchain (ENG_BUILD_DIR, ENG_EXT_DIR)
 
@@ -48,6 +50,7 @@ group "libs"
 bgfxProject("", "StaticLib", os.is("windows") and { "BGFX_CONFIG_RENDERER_DIRECT3D9=1" } or {})
 dofile (BGFX_DIR .. "scripts/example-common.lua")
 assimp_library()
+fswatch_library()
 
 if _OPTIONS["with-tools"] then
     dofile ( "shaderc.lua" )
