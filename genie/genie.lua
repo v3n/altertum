fe5d8c7ce64a61a192889cc77599bf1f065ad02d
Jonathan Howard <j@hovverd.com>
2015-09-20 20:04:04 -0400

    copy libluajit files to bin directory

--- genie/genie.lua
+++ genie/genie.lua
@@ -55,3 +55,14 @@ end
 
 group "engine"
 engine_project("senior", "ConsoleApp", {})
+
+configuration { "vs*" }
+    local win_copy = function(p1, p2) return "xcopy \"" .. path.translate(p1) .. "\" \"" .. path.translate(p2) .. "\" /I /Q /S /E /Y /R" end
+    prelinkcommands
+    {
+        win_copy(ENG_EXT_DIR .. "luajit/lib/win_x64/luajit.exe",    ENG_BUILD_DIR .. "windows/bin"),
+        win_copy(ENG_EXT_DIR .. "luajit/lib/win_x64/lua51.lib",    ENG_BUILD_DIR .. "windows/bin"),
+        win_copy(ENG_EXT_DIR .. "luajit/lib/win_x64/lua51.exp",    ENG_BUILD_DIR .. "windows/bin"),
+        win_copy(ENG_EXT_DIR .. "luajit/lib/win_x64/lua51.dll",    ENG_BUILD_DIR .. "windows/bin"),
+        win_copy(ENG_EXT_DIR .. "luajit/src/jit",                  ENG_BUILD_DIR .. "windows/bin/jit")
+    }
