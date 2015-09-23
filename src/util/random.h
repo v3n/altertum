55b3db416806fa6b1919a6281475c5fd901f51bd
Jonathan Howard <j@hovverd.com>
2015-09-22 23:55:55 -0400

    removed SSE random after some benchmarking (probably don't need such high period)

--- src/util/random.h
+++ src/util/random.h
@@ -3,15 +3,7 @@
  * License: https://github.com/v3n/altertum/blob/master/LICENSE
  */
 
-#include "macros.h"
-
-#ifdef __WITH_SSE
-#include "emmintrin.h"
-#endif
-
-/* sse implementation courtesy Intel: 
-https://software.intel.com/en-us/articles/fast-random-number-generator-on-the-intel-pentiumr-4-processor/
- */
+#include <stdint.h>
 
 namespace altertum {
 class Random
@@ -30,11 +22,7 @@ private:
     void    set_seed( uint32_t );
     uint32_t _rand();
 
-#   ifdef __WITH_SSE
-    __m128i cur_seed __ALIGNED__(16);
-#   else
-    unsigned int _seed;
-#   endif
+    int32_t _seed;
 }; // class Random
 
 inline Random::Random(int32_t seed)
@@ -57,62 +45,6 @@ inline float Random::real()
     return integer() / (float)0x7fff; // preserve sign bit
 }
 
-/**
- * SSE implementation specific 
- */
-
-#ifdef __WITH_SSE
-
-inline void Random::set_seed( uint32_t seed )
-{
-    cur_seed = _mm_set_epi32( seed, seed+1, seed, seed+1 );
-}
-
-inline uint32_t Random::_rand()
-{
-    __m128i cur_seed_split __ALIGNED__(16);
-    __m128i multiplier __ALIGNED__(16);
-    __m128i adder __ALIGNED__(16);
-    __m128i mod_mask __ALIGNED__(16);
-    __m128i sra_mask __ALIGNED__(16);
-    __m128i sseresult __ALIGNED__(16);
-    static const unsigned int mult[4] __ALIGNED__(16) =
-        { 214013, 17405, 214013, 69069 };
-    static const unsigned int gadd[4] __ALIGNED__(16) =
-        { 2531011, 10395331, 13737667, 1 };
-    static const unsigned int mask[4] __ALIGNED__(16) =
-        { 0xFFFFFFFF, 0, 0xFFFFFFFF, 0 };
-    static const unsigned int masklo[4] __ALIGNED__(16) =
-        { 0x00007FFF, 0x00007FFF, 0x00007FFF, 0x00007FFF };
-
-    adder = _mm_load_si128( (__m128i*) gadd);
-    multiplier = _mm_load_si128( (__m128i*) mult);
-    mod_mask = _mm_load_si128( (__m128i*) mask);
-    sra_mask = _mm_load_si128( (__m128i*) masklo);
-    cur_seed_split = _mm_shuffle_epi32( cur_seed, _MM_SHUFFLE( 2, 3, 0, 1 ) );
-
-    cur_seed = _mm_mul_epu32( cur_seed, multiplier );
-    multiplier = _mm_shuffle_epi32( multiplier, _MM_SHUFFLE( 2, 3, 0, 1 ) );
-    cur_seed_split = _mm_mul_epu32( cur_seed_split, multiplier );
-
-    cur_seed = _mm_and_si128( cur_seed, mod_mask);
-    cur_seed_split = _mm_and_si128( cur_seed_split, mod_mask );
-    cur_seed_split = _mm_shuffle_epi32( cur_seed_split, _MM_SHUFFLE( 2, 3, 0, 1 ) );
-    cur_seed = _mm_or_si128( cur_seed, cur_seed_split );
-    cur_seed = _mm_add_epi32( cur_seed, adder);
-
-    /* reduce to RAND_MAX */
-    sseresult = _mm_srai_epi32( cur_seed, 16);
-    sseresult = _mm_and_si128( sseresult, sra_mask );
-
-    return *(uint32_t *)&cur_seed;
-}
-
-#else // !__WITH_SSE
-
-/**
- * non-SSE implementation specific 
- */
 inline void Random::set_seed( uint32_t seed )
 {
     _seed = seed;
@@ -124,6 +56,5 @@ inline uint32_t Random::_rand()
     return (_seed >> 16) & 0x7FFF;
 }
 
-#endif // __WITH_SSE
-
 } // namespace altertum
+
