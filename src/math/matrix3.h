6ea34d2ecf4e9e8d897de2def9e4e78e4d30362a
Jonathan Howard <j@hovverd.com>
2015-09-29 23:17:29 -0400

    math library updates

---
+++ src/math/matrix3.h
@@ -0,0 +1,60 @@
+/* 
+ * Copyright (c) 2015 Jonathan Howard
+ * License: https://github.com/v3n/altertum/blob/master/LICENSE
+ */
+
+#pragma once
+
+#include "math_types.h"
+
+namespace altertum
+{
+
+/**
+ * @file matrix3.h
+ * Extended implementation of Matrix3 API
+ * @note Does not use C++ constructors to maintain aggregate struct status
+ */
+
+/* constructors */
+namespace matrix3
+{
+
+inline Matrix3 matrix3(Vector3 vec[3])
+{
+    Matrix3 tmp;
+
+    tmp.a = vec[0];
+    tmp.b = vec[1];
+    tmp.c = vec[2];
+
+    return tmp;
+}
+
+}; // namespace matrix3
+
+/* implementation */
+namespace matrix3 
+{
+
+inline Matrix3 rotation(const Quaternion& rot)
+{
+    Matrix3 tmp;
+
+    tmp.a.x = 1.0 - (rot.y * rot.y + rot.z * rot.z);
+    tmp.a.y = (rot.x * rot.y - rot.w * rot.z);
+    tmp.a.z = (rot.x * rot.z + rot.w * rot.y);
+
+    tmp.b.x = (rot.x * rot.y + rot.w * rot.z);
+    tmp.b.y = 1.0 - (rot.x * rot.x + rot.z * rot.z);
+    tmp.b.z = (rot.y * rot.z - rot.w * rot.x);
+
+    tmp.c.x = (rot.x * rot.z - rot.w * rot.y);
+    tmp.c.y = (rot.y * rot.z + rot.w * rot.x);
+    tmp.c.z = 1.0 - (rot.x * rot.x + rot.y * rot.y);
+
+    return tmp;
+}
+
+}; // namespace matrix3
+}; // namespace altertum
--- src/math/matrix4.h
+++ src/math/matrix4.h
@@ -7,9 +7,90 @@
 
 #include "math_types.h"
 #include "vector4.h"
+#include "matrix3.h"
 
 namespace altertum
 {
+
+/**
+ * @file matrix4.h
+ * Extended implementation of Matrix4 API
+ * @note Does not use C++ constructors to maintain aggregate struct status
+ */
+
+/* constructors */
+namespace matrix4
+{
+
+inline Matrix4 matrix4(const Vector4 vec[4])
+{
+    Matrix4 tmp;
+
+    tmp.a = vec[0];
+    tmp.b = vec[1];
+    tmp.c = vec[2];
+    tmp.d = vec[3];
+
+    return tmp;
+}
+
+/** Construct Matrix4 from float array. */
+inline Matrix4 matrix4(const float f[16])
+{
+    Matrix4 m;
+
+    m.a.x = f[0];
+    m.a.y = f[1];
+    m.a.z = f[2];
+    m.a.w = f[3];
+
+    m.b.x = f[4];
+    m.b.y = f[5];
+    m.b.z = f[6];
+    m.b.w = f[7];
+
+    m.c.x = f[8];
+    m.c.y = f[9];
+    m.c.z = f[10];
+    m.c.w = f[11];
+
+    m.d.x = f[12];
+    m.d.y = f[13];
+    m.d.z = f[14];
+    m.d.w = f[15];
+
+    return m;
+}
+
+/** Construct Matrix4 from Matrix3 @a mat3. */
+inline Matrix4 matrix4(const Matrix3& mat3)
+{
+    Matrix4 tmp;
+
+    tmp.a.x = mat3.a.x;
+    tmp.a.y = mat3.a.y;
+    tmp.a.z = mat3.a.z;
+    tmp.a.w = 0.0;
+
+    tmp.b.x = mat3.b.x;
+    tmp.b.y = mat3.b.y;
+    tmp.b.z = mat3.b.z;
+    tmp.b.w = 0.0;
+
+    tmp.c.x = mat3.c.x;
+    tmp.c.y = mat3.c.y;
+    tmp.c.z = mat3.c.z;
+    tmp.c.w = 0.0;
+
+    tmp.d.x = 0.0;
+    tmp.d.y = 0.0;
+    tmp.d.z = 0.0;
+    tmp.d.w = 1.0;
+
+    return tmp;
+}
+
+}; // namespace matrix4
 
 /** Return result of comparison of matrices @a m1 and @a m2. */
 inline bool operator==(const Matrix4& m1, const Matrix4& m2)
@@ -52,4 +133,37 @@ inline Matrix4 operator*(Matrix4 m1, const Matrix4& m2)
     return m1;
 }
 
+/* implementation */
+namespace matrix4
+{
+
+/** Construct rotation Matrix from Quarternion @a rot. */
+inline Matrix4 rotation(const Quaternion& rot)
+{
+    return matrix4(matrix3::rotation(rot));
+}
+
+/** Construct transform matrix from @a scale, @a rotation, and @a translation. */
+inline Matrix4 compose(const Vector3& scale, const Quaternion& rotation, const Vector3& translation)
+{
+    Matrix4 tmp;
+
+    /* scale */
+    tmp.a.x = scale.x;
+    tmp.b.y = scale.y;
+    tmp.c.z = scale.z;
+    tmp.d.w = 1.0;
+
+    /* rotation */
+    tmp *= matrix4::rotation(rotation);
+
+    /* translation */
+    tmp.a.z += translation.x;
+    tmp.b.z += translation.y;
+    tmp.c.z += translation.z;
+
+    return tmp;
+}
+
+}; // namespace matrix4
 }; // namespace altertum
