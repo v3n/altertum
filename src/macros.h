729ec2d99c21fe43dcd9431680fcaef3679008fa
Jonathan Howard <j@hovverd.com>
2015-10-04 16:34:28 -0400

    macro updates

--- src/macros.h
+++ src/macros.h
@@ -1,13 +1,24 @@
+/* 
+ * Copyright (c) 2015 Jonathan Howard
+ * License: https://github.com/v3n/altertum/blob/master/LICENSE
+ */
+
+#pragma once
+
 #include <cassert>
+
+#include "platform.h"
 
 /* allows changing later without major refactor */
 #define ASSERT(a) assert(a)
 
 /* alignment macros */
-#if defined(_MSC_VER)
-#define __ALIGNED__(x) __declspec(align(x))
+#if ALTERTUM_COMPILER_GCC || ALTERTUM_COMPILER_CLANG
+#   define __ALIGNED__(x)      __attribute__ ((aligned(x)))
+#   define __FORCE_INLINE__    inline  __attribute__ ((always_inline))
+#elif ALTERTUM_COMPILER_MSVC
+#   define __ALIGNED__(x)      __declspec(align(x))
+#   define __FORCE_INLINE__    __forceinline
 #else
-#if defined(__GNUC__)
-#define __ALIGNED__(x) __attribute__ ((aligned(x)))
-#endif
+#   error "Unknown compiler ALTERTUM_COMPILER_?"
 #endif
