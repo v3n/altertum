3b24d2bd52217906901648dc003513e6b6391716
Jonathan Howard <j@hovverd.com>
2015-12-02 23:08:44 -0500

    vector4 constructors

--- src/math/vector4.h
+++ src/math/vector4.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (c) 2015 Jonathan Howard
  * License: https://github.com/v3n/altertum/blob/master/LICENSE
  */
@@ -7,8 +7,30 @@
 
 #include "math_types.h"
 
-namespace altertum 
+namespace altertum
 {
+
+/**
+ * @file vector4.h
+ * Extended implementation of Vector4 API
+ * @note Does not use C++ constructors to maintain aggregate struct status
+ */
+
+/* constructors */
+namespace vector4
+{
+    /** Construct Vector4 from Vector3 @a v */
+    inline Vector4 vector4(Vector3& v)
+    {
+        return { v.x, v.y, v.z, 0.0f };
+    }
+
+    /** Construct Vector4 from components */
+    inline Vector4 vector4(float x, float y, float z, float w)
+    {
+        return { x, y, z, w };
+    }
+}
 
 /** Returns result of comparison of vectors @a v1 and @a v2. */
 inline bool operator==(const Vector4& v1, const Vector4& v2)
